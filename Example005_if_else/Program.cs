ffa19d9ae0f94ea6be197086930abf37c15f2fa3
Timur <kokanbaev1@gmail.com>
2023-04-19 19:44:36 +0300

    add if/else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "Маша") 
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
